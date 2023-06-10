153c572dcf8a56d13744b777589de4b3bd35d183
DouShaoxun <dousx_cn@163.com>
2023-06-10 18:51:11 +0800

    自定义线程池

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::{fs, thread};
+use std::{fs};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use rust_simple_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8564").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        //  针对每一个连接建立一个线程
-        thread::spawn(|| {
+        pool.execute(|| {
+            // 任务提交到线程池
             handle_connection(stream);
         });
     }
