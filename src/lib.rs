c5e83cdb25cf4516d0eca4a8290a5e89152b6d63
DouShaoxun <dousx_cn@163.com>
2023-06-10 18:53:58 +0800

    优雅停机

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,87 @@
-use std::sync::{Arc, mpsc, Mutex};
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
-
-trait FnBox {
-    fn call_box(self: Box<Self>);
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 }
+// --snip--
 
-impl<F: FnOnce()> FnBox for F {
-    fn call_box(self: Box<Self>) {
-        (*self)()
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        // --snip--
+
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 
-type Job = Box<dyn FnBox + Send + 'static>;
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job", id);
-            job.call_box();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
         });
-        Worker { id, thread }
-    }
-}
 
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
-}
-
-impl ThreadPool {
-    /// 创建一个线程池
-    ///
-    /// size 必须大于0
-    pub fn new(size: usize) -> ThreadPool {
-        assert!((size > 0));
-        // 通道
-        let (sender, receiver) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
-        let mut workers = Vec::with_capacity(size);
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)))
+        Worker {
+            id,
+            thread: Some(thread),
         }
-        ThreadPool { workers, sender }
-    }
-
-    pub fn len(&self) -> usize {
-        self.workers.len()
-    }
-    pub fn execute<F>(&self, f: F)
-        where F: FnOnce() + Send + 'static {
-        let job = Box::new(f);
-        self.sender.send(job).unwrap();
     }
 }
-
